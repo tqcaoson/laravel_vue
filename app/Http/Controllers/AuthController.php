30a39b1a6af130f5e04c19fa9e3f7afdd4126060
tqcaoson <57176427+tqcaoson@users.noreply.github.com>
2021-06-27 00:48:49 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -116,7 +116,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
+            'expires_in' => auth()->factory()->getTTL() * (60*24*2),
             'name' => auth()->user()->name,
             'user_id' => auth()->user()->id,
             'email' => auth()->user()->email,
